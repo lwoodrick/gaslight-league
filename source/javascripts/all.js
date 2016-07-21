99e4ecc39d72320aec918644c0b17184c5334e1a
lwoodrick <lauren@teamgaslight.com>
2016-07-21 15:23:10 -0400

    Make miscellaneous look/feel updates.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 // This is where it all goes :)
+
+window.onload = init;
+function init() {
+  document.getElementById('small-ranking').onclick = closeLargeRanking;
+  document.getElementById('large-ranking').onclick = openLargeRanking;
+}
+
+function closeLargeRanking() {
+  document.getElementById('rank-toggle').setAttribute('class','open');
+}
+
+function openLargeRanking() {
+  document.getElementById('rank-toggle').setAttribute('class','');
+}
